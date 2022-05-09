ecafc9b063016448d208eec7f58c924726e71164
Enzo7121 <enzo.lanus@gmail.com>
2022-05-09 13:33:12 -0300

    classes exercise

---
+++ classes.py
@@ -0,0 +1,32 @@
+import doctest as dt
+
+
+class Circle:
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius cannot be negative")
+        self.radius = radius
+
+    def area(self):
+        area = round(self.radius ** 2 * 3.14, 1)
+        return print('The area of the circle is: {}'.format(area))
+
+    def perimeter(self):
+        perimeter = round(2 * 3.14 * self.radius, 1)
+        return print('The perimeter of the circle is: {}'.format(perimeter))
+
+
+# create a test case for the class Circle with doctest
+def test_circle():
+    """
+    >>> c = Circle(5)
+    >>> c.area()
+    The area of the circle is: 78.5
+    >>> c.perimeter()
+    The perimeter of the circle is: 31.4
+    """
+    dt.run_docstring_examples(test_circle, globals(), True)
+
+
+if __name__ == '__main__':
+    test_circle()
