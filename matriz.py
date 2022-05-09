6766abff69ece9908ec09f9a012c40c724581503
Enzo7121 <enzo.lanus@gmail.com>
2022-05-09 13:16:31 -0300

    matriz ejercicio

---
+++ matriz.py
@@ -0,0 +1,29 @@
+import random
+
+
+def generateMatrix():
+    list = []
+    for i in range(5):
+        list.append([])
+        for j in range(5):
+            list[i].append(random.randint(5, 10))
+    return list
+
+
+def findSequence(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == matrix[i][j]:
+                if i < len(matrix) - 3:
+                    if matrix[i + 1][j] == matrix[i][j] + 1 and matrix[i + 2][j] == matrix[i][j] + 2 and matrix[i + 3][j] == matrix[i][j] + 3:
+                        return print('Sequence starts at: {}, {} and ends at: {}, {}'.format(i, j, i + 3, j))
+                if j < len(matrix[i]) - 3:
+                    if matrix[i][j + 1] == matrix[i][j] + 1 and matrix[i][j + 2] == matrix[i][j] + 2 and matrix[i][j + 3] == matrix[i][j] + 3:
+                        return print('Sequence starts at: {}, {} and ends at: {}, {}'.format(i, j, i, j + 3))
+    return print('No sequence found')
+
+
+list = generateMatrix()
+
+
+print(findSequence(list))
