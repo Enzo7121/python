04c339235f400087fffe1492b35a0360cf4ea0df
Enzo7121 <enzo.lanus@gmail.com>
2022-05-09 11:06:38 -0300

    simple

---
+++ simple.py
@@ -0,0 +1,19 @@
+import random
+
+
+def randomNumber():
+    return random.randint(1, 100)
+
+
+def genList():
+    list = [{'id': 1, 'age': randomNumber()}, {'id': 2, 'age': randomNumber()}, {
+        'id': 3, 'age': randomNumber()}, {'id': 4, 'age': randomNumber()}, {'id': 5, 'age': randomNumber()}, {'id': 6, 'age': randomNumber()}, {'id': 7, 'age': randomNumber()}, {'id': 8, 'age': randomNumber()}, {'id': 9, 'age': randomNumber()}, {'id': 10, 'age': randomNumber()}]
+    return list
+
+
+def sortListHighToLow(list):
+    list.sort(key=lambda x: x['age'], reverse=True)
+    return list
+
+
+print(sortListHighToLow(genList()))
